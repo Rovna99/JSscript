0dbd51d2d22120bca3041530098af208ced5d85e
Rovna99 <tjdalswodi91@gmail.com>
2021-04-07 20:17:56 +0900

    수정

--- clock.js
+++ clock.js
@@ -5,7 +5,7 @@ function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  const seconds = date.getSeconds();
+  // const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }`;
